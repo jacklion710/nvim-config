b17f68b092659f2c754f41e9f6a47e5f4f7cdeb6
jacklion710 <jack.lion710@gmail.com>
2024-08-07 01:30:10 -0400

    Setting Conda env instructions

--- lua/jake/set-conda.lua
+++ lua/jake/set-conda.lua
@@ -1,1 +1,8 @@
+-- If using a Conda env ensure this path is correct
+-- Install the pynvim package into your Conda env
+-- $ pip install pynvim
+-- To get the path activate your conda env and run the following command:
+-- $ which python
+-- Copy the path to the string below
+-- If using WSL ensure that you have Conda installed in your subsystem
 vim.g.python3_host_prog = "/Users/jacobleone/miniconda3/envs/pynvim/bin/python"
